ab429354444a303ce440e4c6763c31ef09c3edd0
HaozheSong <haozhes@andrew.cmu.edu>
2023-10-21 22:08:47 +0000

    task-1.6.6 display local time

--- frontend/src/app/dashboard/task-status/page.tsx
+++ frontend/src/app/dashboard/task-status/page.tsx
@@ -27,5 +27,23 @@ async function getAllTasks (
 ) {
   const response = await fetch('/api/task/read/all/')
   const allTasks: Array<Task> = await response.json()
+  for (let task of allTasks) {
+    task.created_at = toLocalTime(task.created_at)
+    task.updated_at = toLocalTime(task.updated_at)
+  }
   setAllTasks(allTasks)
+}
+
+function toLocalTime (utcString: string) {
+  const time = new Date(utcString)
+  const padZero = (num: number) => (num < 10 ? `0${num}` : num)
+
+  const month = padZero(time.getMonth() + 1)
+  const date = padZero(time.getDate())
+  const hour = padZero(time.getHours())
+  const minute = padZero(time.getMinutes())
+  const second = padZero(time.getSeconds())
+
+  const localTimeStr = `${time.getFullYear()}${month}${date}-${hour}${minute}${second}`
+  return localTimeStr
 }
